860bb2e0306d5f94953b2d6283b5440826c9b107
Zack Creach <zackcreach@gmail.com>
2024-06-05 20:43:06 -0400

    please to the stage, stevie nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Cool Beans";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+		home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Zacks-MacBook-Pro
+    darwinConfigurations."Zacks-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [
+				./hosts/Zacks-MacBook-Pro.nix 
+				home-manager.darwinModules.home-manager
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.users.zack = import ./home/zack.nix;
+					home-manager.extraSpecialArgs = { inherit inputs; };
+				}
+			];
+			specialArgs = { inherit inputs self; };
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."Zacks-MacBook-Pro".pkgs;
+  };
+}
---
+++ hosts/Zacks-MacBook-Pro.nix
@@ -0,0 +1,32 @@
+{ pkgs, self, ... }: 
+{
+	# List packages installed in system profile. To search by name, run:
+	# Auto upgrade nix package and the daemon service.
+	services.nix-daemon.enable = true;
+	# nix.package = pkgs.nix;
+
+	# Necessary for using flakes on this system.
+	nix.settings.experimental-features = "nix-command flakes";
+
+	# Create /etc/zshrc that loads the nix-darwin environment.
+	programs.zsh.enable = true;  # default shell on catalina
+	# programs.fish.enable = true;
+
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+			allowUnsupportedSystems = true;
+		};
+
+		hostPlatform = "aarch64-darwin";
+	};
+
+	# Set Git commit hash for darwin-version.
+	system.configurationRevision = self.rev or self.dirtyRev or null;
+
+	# Used for backwards compatibility, please read the changelog before changing.
+	# $ darwin-rebuild changelog
+	system.stateVersion = 4;
+
+	users.users.zack.home = "/Users/zack";
+}
