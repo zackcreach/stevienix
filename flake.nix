a577e45d0bf5466807007997a11488b8a2c50e63
Zack Creach <zackcreach@gmail.com>
2024-08-30 12:43:32 -0400

    started windows iso flake

--- flake.nix
+++ flake.nix
@@ -55,5 +55,25 @@
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."Zacks-MacBook-Pro".pkgs;
+
+      nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ({ pkgs, ... }: {
+            environment.systemPackages = with pkgs; [
+              neovim
+              git
+              networkmanager
+              gptfdisk
+            ];
+
+            systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
+            users.users.root.openssh.authorizedKeys.keys = [
+              "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDjV8sPKVLfZWddqPBv9jfVuLlUYP0PLr+lXvbKoPLe8xX5Nt5Ch0T9GW6D5szQBsZHwQY6OUhhBuAQqcJFXGJjolvVdeTA82N5Wg/DZVG8gS8eAkiQbkj6f6hm4rMie7BUm0HYdZUodygtQFoISQb3G5Tw8KfbWfZHojT1Pdgrof5M2zLewMzBiWmL1iB3IkzMlirltHITe9ukzKqeM/InVecSUJ8BmNiflsF3TXtbLxQGsTLRE1mG50rsUonZogxzGDaKlHEd4raeM1kleyrSpwFGJJGQYz7jckwP0z/VlJeZ0/DYVT/tm8GzRlPknx1d2cCMFaeqqAk4IXcBS2ooYBdn7jQm0/GvAY6no+Cp1NiYoVDegCL2af2EthMNhrgaIvWWEIZXjEqcsjjOSAaoG551yb344+HQVesteg9Gx7+uyAbvMBLko5pC49rV99czPC3btDCLX0z5DhAh4eP0wbMrkutjFmNRe07F5NAeROLJZK5g7lrm76DCZ/lZwBc= zackcreach@gmail.com"
+            ];
+          })
+        ];
+      };
     };
 }
