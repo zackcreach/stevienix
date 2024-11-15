03bd99ba88a165f50b7e419ca808aca7940b66d5
Zack Creach <zackcreach@gmail.com>
2024-11-15 10:45:30 -0500

    consolidated scratchpad and dadbod files into creachignore folder

--- home/editor/neovim/lua/plugins/db.lua
+++ home/editor/neovim/lua/plugins/db.lua
@@ -7,7 +7,7 @@ return {
 	init = function()
 		vim.g.db_ui_use_nerd_fonts = true
 		vim.g.db_ui_execute_on_save = false
-		vim.g.db_ui_save_location = "./creachqueries"
+		vim.g.db_ui_save_location = "./creachignore"
 		vim.g.db_ui_disable_mappings = true
 	end,
 }
