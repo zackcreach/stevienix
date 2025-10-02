b7e28e76a2ceebff7d897effd84ba2fef1428873
Zack Creach <zackcreach@gmail.com>
2025-10-02 07:34:32 -0400

    updated opencode models and provider

--- home/ai/client.nix
+++ home/ai/client.nix
@@ -50,8 +50,8 @@
     opencode = {
       enable = true;
       settings = {
-        model = "ollama/Qwen3-Coder-30B-A3B";
-        small_model = "ollama/gemma3:27b";
+        model = "zionlab/gpt-oss:120b";
+        small_model = "zionlab/qwen3-coder:30b-a3b";
         permission = {
           "*" = "ask";
           edit = "ask";
@@ -63,21 +63,29 @@
           pwd = "allow";
         };
         theme = "nord";
+        keybinds = {
+          input_newline = "enter";
+          input_submit = "ctrl+y";
+        };
         provider = {
-          ollama = {
+          zionlab = {
             npm = "@ai-sdk/openai-compatible";
             options = {
-              baseURL = "http://symphony:11434/v1";
+              baseURL = "https://ollama-api.zionlab.online/v1";
+              headers = {
+                CF-Access-Client-Secret = "{env:CF_ACCESS_CLIENT_SECRET}";
+                CF-Access-Client-Id = "{env:CF_ACCESS_CLIENT_ID}";
+              };
             };
             models = {
-              "Qwen3-Coder-30B-A3B" = {
+              "qwen3-coder:30b-a3b" = {
                 tools = true;
                 reasoning = false;
                 options = {
                   num_ctx = 16534;
                 };
               };
-              "gemma3:27b" = {
+              "gpt-oss:120b" = {
                 tools = false;
                 reasoning = false;
                 options = {
